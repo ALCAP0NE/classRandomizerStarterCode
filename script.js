77f5e3d30a1cfb42413a7deaad11dd1f6cb25820
Alan Capon <alanyc522@gmail.com>
2017-05-04 00:51:04 +0000

    Random

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+    var group = ["Alan", "Joel", "Yanella", "Josue", "Tushar", "Odalys"];
+    var ScriptEducators = ["Aaron", "Justin", "Zach", "Julia", "Alyxe"];
+    $("#studentButton").click(function() {
+        var students = Math.floor(Math.random()*group.length);
+        $("#studentDisplay").append(group[students]);
 
+    });
+$("#teacherButton").click(function() {
+    var teachers = Math.floor(Math.random()*ScriptEducators.length);
+    $("#teacherDisplay").append(ScriptEducators[teachers]);
+});
 
 
 });
